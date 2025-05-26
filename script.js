cdf383012073a726050af5d483f46ef0258c1f26
Vincenzo Parente <vincenzo.parente@eng.it>
2025-05-26 16:24:55 +0200

    definitivo

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener('scroll', function() {
     const windowHeight = window.innerHeight;
     // Altri elementi nascosti (es. scritte)
@@ -20,27 +19,4 @@ window.addEventListener('load', function () {
 window.addEventListener('resize', () => {
   // Rilancia tutto se necessario
   window.dispatchEvent(new Event('scroll'));
-=======
-window.addEventListener('scroll', function() {
-    const windowHeight = window.innerHeight;
-    // Altri elementi nascosti (es. scritte)
-    const scritte = document.querySelectorAll('.scritta-nascosta');
-
-    scritte.forEach(function(el) {
-        const position = el.getBoundingClientRect().top;
-        if (position < windowHeight - 100) {
-            el.classList.add('visibile');
-        }
-    });
-});
-    
-// In caso siano già visibili al caricamento
-window.addEventListener('load', function () {
-    window.dispatchEvent(new Event('scroll'));
-});
-
-window.addEventListener('resize', () => {
-  // Rilancia tutto se necessario
-  window.dispatchEvent(new Event('scroll'));
->>>>>>> c7cdcc7813865e06aa6dc4ebb9fbdea8d7764a2a
 });
