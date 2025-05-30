82673bbd99b47f79c106f0c1e266ebe659219fc1
VincenzoPar <vincenzo.parente@eng.it>
2025-05-30 16:18:38 +0200

    Aggiunto menù

---
+++ scriptMenu.js
@@ -0,0 +1,32 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleziona tutti i bottoni
+    const buttons = document.querySelectorAll(".btn-menu");
+
+    document.getElementById('menu-title').textContent = "I FRITTI";
+    document.getElementById('menu-image').src = "img/menu/fritti.png"
+
+    buttons.forEach(button => {
+      button.addEventListener("click", function() {
+        // Rimuove la classe active da tutti i bottoni
+        buttons.forEach(btn => btn.classList.remove("active"));
+        // Aggiunge la classe active al bottone cliccato
+        this.classList.add("active");
+
+        //Legge i dati personalizzati dal bottone
+  
+        const title = this.getAttribute('data-title');
+        const imageSrc = this.getAttribute('data-image');
+
+        //Cambia il menù
+        changeMenu(title, imageSrc);
+      });
+    });
+
+    // La tua funzione esistente
+    function changeMenu(title, imageSrc) {
+      console.log("Cambio menu:", title, imageSrc);  // 👈 verifica cosa arriva
+      document.getElementById('menu-title').textContent = title;
+      document.getElementById('menu-image').src = imageSrc;
+    }
+  });
